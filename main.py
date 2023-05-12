966967377ae296688db97367680eb269cef3e4d9
Vasanth <88794529+VasanthK29@users.noreply.github.com>
2023-05-12 07:33:41 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -32,17 +32,17 @@ def loc_changed(json):
 	print("received json: "+str(coordinates['latitude'])+" "+str(coordinates['longitude']))
 	user_lat = coordinates['latitude']
 	user_lon = coordinates['longitude']
-	rajaji_lat = 9.928169944748596
-	rajaji_lon = 78.12945702961692
+	rajaji_lat = 9.927876668067677
+	rajaji_lon = 78.13048097430466
 	dbService = databaseService()
 	junction = dbService.getTableJunction();
 	count = 0
-	array = [9.932213, 78.093467]
+	array = [9.928265, 78.130602]
 	#pt1 = (user_lat, user_lon)
 	pt1 = (array[0],array[1])
-	pt2 = (rajaji_lat,rajaji_lon)
-	dt = haversine(point1, point2, unit=Unit.KILOMETERS)
-	dtm = distance * 1000
+	pt2 = (rajaji_lat,rajaji_lon)	
+	dt = haversine(pt1, pt2, unit=Unit.KILOMETERS)
+	dtm = dt * 1000
 	if(dtm<100):
 		print("near rajaji hospital")
 	else:
@@ -54,7 +54,7 @@ def loc_changed(json):
 			distance = haversine(point1, point2, unit=Unit.KILOMETERS)
 			distance_in_m = distance * 1000
 			if(int(distance_in_m)<200):
-				print(x.junctionName)
+				print("near  " + x.junctionName + "  switching signal for 30 seconds...")
 				count = 1
 		if(count==0):
 			print("yet to arrive near signal")
